ee7c47eaadc7ef3527fafe8ee06ce51ee9a82d20
rflperry <rflperry@gmail.com>
2021-02-26 14:17:42 -0500

    updated figs/scores

--- benchmarks/uf_experiments/cc18/run_all.py
+++ benchmarks/uf_experiments/cc18/run_all.py
@@ -2,6 +2,8 @@ import numpy as np
 import openml
 import pickle
 import logging
+import time
+import os
 
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.calibration import CalibratedClassifierCV
@@ -12,18 +14,20 @@ from sklearn.preprocessing import OneHotEncoder
 from proglearn.forest import UncertaintyForest
 from sklearn.impute import SimpleImputer
 
+MODE = 'APPEND'
+
 n_estimators = 500
 uf_kappa = 1
 up_construction_prop = 0.5
-
 n_jobs = 30
+cv = 10
 
 clfs = [
     (
         "IRF",
         CalibratedClassifierCV(
             base_estimator=RandomForestClassifier(
-                n_estimators=n_estimators, n_jobs=n_jobs
+                n_estimators=n_estimators // 5, n_jobs=n_jobs
             ),
             method="isotonic",
             cv=5,
@@ -33,30 +37,49 @@ clfs = [
         "SigRF",
         CalibratedClassifierCV(
             base_estimator=RandomForestClassifier(
-                n_estimators=n_estimators, n_jobs=n_jobs
+                n_estimators=n_estimators // 5, n_jobs=n_jobs
             ),
             method="sigmoid",
             cv=5,
         ),
     ),
-    (
-        "UF",
-        UncertaintyForest(
-            n_estimators=n_estimators,
-            tree_construction_proportion=up_construction_prop,
-            kappa=uf_kappa,
-        ),
-    ),
-    ("RF", RandomForestClassifier(n_estimators=n_estimators, n_jobs=n_jobs)),
+    # (
+    #     "UF",
+    #     UncertaintyForest(
+    #         n_estimators=n_estimators,
+    #         tree_construction_proportion=up_construction_prop,
+    #         kappa=uf_kappa,
+    #     ),
+    # ),
+    # ("RF", RandomForestClassifier(n_estimators=n_estimators, n_jobs=n_jobs)),
 ]
 
+def _check_nested_equality(lst1, lst2):
+    if isinstance(lst1, list) and isinstance(lst2, list):
+        for l1, l2 in zip(lst1, lst2):
+            if not _check_nested_equality(l1, l2):
+                return False
+    elif isinstance(lst1, np.ndarray) and isinstance(lst2, np.ndarray):
+        return np.all(lst1 == lst2)
+    else:
+        return lst1 == lst2
+    
+    return True
 
 def train_test(X, y, task_name, nominal_indices):
-    cv = 10
+    save_path = f"./results_cv{cv}/{task_name}_results_dict.pkl"
+    if MODE == 'APPEND':
+        if not os.path.isfile(save_path):
+            logging.info(f'APPEND MODE: Skipping {task_name}')
+            return
+
+    # Set up Cross validation
+    
     skf = StratifiedKFold(n_splits=cv, shuffle=True, random_state=0)
     n_samples, n_features = X.shape
     n_classes = len(np.unique(y))
 
+    # Check if existing experiments
     results_dict = {
         "task": task_name,
         "task_id": task_id,
@@ -68,7 +91,7 @@ def train_test(X, y, task_name, nominal_indices):
         "n_estimators": n_estimators,
         "cv": cv,
         "nominal_features": len(nominal_indices),
-        "UF_params": {
+        "UF_metadata": {
             "tree_construction_proportion": up_construction_prop,
             "kappa": uf_kappa,
             "n_estimators": n_estimators,
@@ -105,17 +128,49 @@ def train_test(X, y, task_name, nominal_indices):
             ('Estimator', clf)])
 
         fold_probas = []
+        if not f"{clf_name}_metadata" in results_dict.keys():
+            results_dict[f"{clf_name}_metadata"] = {}
+        results_dict[f"{clf_name}_metadata"]["train_times"] = []
+        results_dict[f"{clf_name}_metadata"]["test_time"]= []
         for train_index, test_index in skf.split(X, y):
             X_train, X_test = X[train_index], X[test_index]
             y_train, y_test = y[train_index], y[test_index]
 
+            start_time = time.time()
             pipeline = pipeline.fit(X_train, y_train)
+            train_time = time.time() - start_time
+
             y_proba = pipeline.predict_proba(X_test)
+            test_time = time.time() - (train_time + start_time)
+
             fold_probas.append(y_proba)
+            results_dict[f"{clf_name}_metadata"]["train_times"].append(train_time)
+            results_dict[f"{clf_name}_metadata"]["test_time"].append(test_time)
         
         results_dict[clf_name] = fold_probas
 
-    with open(f"./results_cv10/{task_name}_results_dict.pkl", "wb") as f:
+    # If existing data, load and append to. Else save
+    if os.path.isfile(save_path):
+        logging.info(f'Existing data for {task_name} ({task_id}), appending')
+        with open(save_path, "rb") as f:
+            prior_results = pickle.load(f)
+
+        # Check these keys have the same values
+        verify_keys = [
+            "task", "task_id", "n_samples", "n_features", "n_classes", "y",
+            "test_indices", "n_estimators", "cv", "nominal_features"]
+        for key in verify_keys:
+            assert _check_nested_equality(prior_results[key], results_dict[key]), key
+
+        # Replace/add data
+        replace_keys = [name for name, _ in clfs]
+        replace_keys += [f"{name}_metadata" for name in replace_keys]
+        for key in replace_keys:
+            prior_results[key] = results_dict[key]
+
+        results_dict = prior_results
+
+    with open(save_path, "wb") as f:
         pickle.dump(results_dict, f)
 
 
@@ -132,7 +187,7 @@ for task_id in benchmark_suite.tasks:  # iterate over all tasks
     #     logging.info(f'Skipping {task_name} ({task_id})')
     #     continue
     # else:
-    print(f'Running {task_name} ({task_id})')
+    print(f'{MODE} {task_name} ({task_id})')
     logging.info(f'Running {task_name} ({task_id})')
 
     X, y = task.get_X_and_y()  # get the data
@@ -145,4 +200,3 @@ for task_id in benchmark_suite.tasks:  # iterate over all tasks
     except Exception as e:
         logging.error(f'Test {task_name} ({task_id}) Failed | X.shape={X.shape} | {len(nominal_indices)} nominal indices')
         logging.error(e)
-
